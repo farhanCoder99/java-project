c69797f3617e859ba51b64c321473dc60a23926f
farhan khan <131686214+farhanCoder99@users.noreply.github.com>
2023-06-05 16:50:07 +0530

    loops

---
+++ age.java
@@ -0,0 +1,20 @@
+package nov5_22;
+
+public class age {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int age=23;
+		if(age>=18){
+			System.out.println("vote dega");
+			
+		}else {
+			System.out.println("vote dega");
+			
+			
+		}
+		
+
+	}
+
+}
