6e4bd331c598d348419d87eb8cefa62a213a2cbd
farhan khan <131686214+farhanCoder99@users.noreply.github.com>
2023-06-05 17:02:13 +0530

    Add files via upload

---
+++ maximum_No.java
@@ -0,0 +1,21 @@
+package nov5_22;
+
+public class maximum_No {
+
+	public static void main(String[] args) {
+	
+		// TODO Auto-generated method stub
+		int a=22;
+		int b=33;
+		int c=44;
+		if(a>b && b>c) {
+			System.out.println(a);
+		}else if(b>a && b>c) {
+			System.out.println(b);
+		}else {
+			System.out.println(c);
+		}
+
+	}
+
+}
