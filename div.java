45801a972eed25c80866d598a8d8a43bc809a1a8
farhan khan <131686214+farhanCoder99@users.noreply.github.com>
2023-06-05 16:59:23 +0530

    Add files via upload

---
+++ div.java
@@ -0,0 +1,16 @@
+package nov5_22;
+
+public class div {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int n=56;
+		if(n % 3==0 && n %4 ==0) {
+			System.out.println("yes");
+		}else {
+			System.out.println("no");
+		}
+
+	}
+
+}
